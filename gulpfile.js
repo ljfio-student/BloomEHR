3110548aaa98b27a83711a65412dad3bcbdf36b5
Luke Fisher <mail@lukefisher.me>
2018-03-18 00:18:07 +0100

    Pipe to output folder

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const tsProject = ts.createProject('tsconfig.json');
 //task to be run when the watcher detects changes
 gulp.task('scripts', () => {
   const tsResult = tsProject.src().pipe(tsProject());
-  return tsResult.js.pipe(gulp.dest(''));
+  return tsResult.js.pipe(gulp.dest('output'));
 });
 //set up a watcher to watch over changes
 gulp.task('watch', ['scripts'], () => {
